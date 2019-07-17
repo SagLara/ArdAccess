0cac3644123dfa0d8e6a1d156271593b6750a0aa
Sergio Gomez <42383087+SagLara@users.noreply.github.com>
2019-07-16 23:41:45 -0500

    Add files via upload

---
+++ prueba/enviardatos.php
@@ -0,0 +1,31 @@
+<?php
+
+$enlace = mysqli_connect("localhost", "root", "", "login");
+
+if (!$enlace) {
+    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos login es genial." . PHP_EOL;
+$enlace->set_charset('utf8');
+
+$chipid = $enlace->real_escape_string($_POST ['chipid']);
+$temperatura = $enlace->real_escape_string($_POST ['temperatura']);
+
+$sql ="INSERT INTO registro (id, chipId, fecha, temperatura) VALUES (NULL, '$chipid', CURRENT_TIMESTAMP, '$temperatura')";
+
+
+if (mysqli_query($enlace, $sql)) {
+      echo "New record created successfully";
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($enlace);
+}
+
+mysqli_close($enlace);
+
+echo "Datos ingresados correctamente.";
+?>
+
